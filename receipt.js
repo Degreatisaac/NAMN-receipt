5255f42f034ebac507cfd646e4b787a4c34052ed
degreatisaac <okeisaacoluwasesangreat1@gmail.com>
2023-05-03 14:42:48 +0100

    first commit

---
+++ receipt.js
@@ -0,0 +1,43 @@
+let user_name = sessionStorage.getItem("name");
+document.querySelector("span.name").innerHTML = user_name;
+
+let user_matric = sessionStorage.getItem("matric");
+document.querySelector("span.matric").innerHTML = user_matric;
+
+let user_level = sessionStorage.getItem("level");
+document.querySelector("span.level").innerHTML = user_level;
+
+let user_status = sessionStorage.getItem("status");
+document.querySelector("span.status").innerHTML = user_status;
+
+let user_amount = sessionStorage.getItem("amount");
+document.querySelector("span.amount").innerHTML = user_amount;
+
+let user_bank = sessionStorage.getItem("bank");
+document.querySelector("span.bank").innerHTML = user_bank;
+
+let user_date = sessionStorage.getItem("date");
+document.querySelector("span.date").innerHTML = user_date;
+
+function submitForm(e) {
+    e.preventDefault();
+    let name = document.forms["welcome_form"]["name"].value;
+    let matric = document.forms["welcome_form"]["matric"].value;
+    let level = document.forms["welcome_form"]["level"].value;
+    let status = document.forms["welcome_form"]["status"].value;
+    let amount = document.forms["welcome_form"]["amount"].value;
+    let bank = document.forms["welcome_form"]["bank"].value;
+    let date = document.forms["welcome_form"]["date"].value;
+ 
+    
+    sessionStorage.setItem("name", name);
+    sessionStorage.setItem("matric", matric);
+    sessionStorage.setItem("level", level);
+    sessionStorage.setItem("status", status);
+    sessionStorage.setItem("amount", amount);
+    sessionStorage.setItem("bank", bank);
+    sessionStorage.setItem("date", date);
+  
+  
+    location.href = "receipt.html";
+  }
